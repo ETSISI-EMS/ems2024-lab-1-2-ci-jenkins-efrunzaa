9d25e29d9f6db49f5600a89db9ee65126719bbc9
br0121 <e.frunza@alumnos.upm.es>
2024-02-15 12:47:42 +0100

    cambio

--- src/test/java/es/upm/etsisi/ems/HelloControllerTest.java
+++ src/test/java/es/upm/etsisi/ems/HelloControllerTest.java
@@ -1,12 +1,12 @@
 package es.upm.etsisi.ems;
 
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest(classes = MyWebApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 //@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -17,7 +17,7 @@ public class HelloControllerTest {
 
     @Test
     public void holaOk() throws Exception {
-        ResponseEntity<String> response = template.getForEntity("/hola", String.class);
+        ResponseEntity<String> response = this.template.getForEntity("/hola", String.class);
         
         // check that the body contains a <p> tag with the text "¡Hola Mundo!"
         assertThat(response.getBody()).contains("<p>¡Hola Mundo!</p>");
@@ -25,9 +25,9 @@ public class HelloControllerTest {
 
     @Test
     public void holaUser() throws Exception {
-        ResponseEntity<String> response = template.getForEntity("/hola?nombre=EMSAlumno", String.class);
+        ResponseEntity<String> response = this.template.getForEntity("/hola?nombre=EMSAlumno", String.class);
 
         // check that the body contains a <p> tag with the text "¡Hola EMSAlumno!"
-        assertThat(response.getBody()).contains("<p>¡Hola EMSAlumno!</p>");
+        assertThat(response.getBody()).contains("<p>¡Hola EMSAlumno ERROR!</p>");
     }
 }
